d87e3f3de9c9cd6f508f4ffb81e09c26009aed84
Martin Trobäck <martintk@axis.com>
2015-07-08 17:09:51 +0200

    Added removeDuplicates

--- snippets.js
+++ snippets.js
@@ -3,5 +3,27 @@ var s_ = function () {
 
 	return {
 		// Public variables and functions
+
+		removeDuplicates: function (arr){
+			// Removes duplicate entries in an array of 
+      // numbers and/or strings
+			if (!(arr instanceof Array) || arr.length < 1){
+				throw new Error('This is not an array.');
+				return arr;
+			}
+
+			var arrayMap = {};
+
+			for (var i = 0; i < arr.length; i++){
+				arrayMap[arr[i]] = arr[i];
+			}
+
+			arr.splice(0, arr.length);
+
+			for (var p in arrayMap){
+				arr.push(arrayMap[p]);
+			}
+			return arr;
+		}
 	}
 }
