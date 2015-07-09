a76d20cbdbde809bf4b7ec3653f1bba341c04fcb
Martin Trobäck <martintk@axis.com>
2015-07-09 08:38:59 +0200

    Make it actually work

--- snippets.js
+++ snippets.js
@@ -1,29 +1,29 @@
-var s_ = function () {
-	// Private variables and functions
+var s_ = (function () {
+  // Private variables and functions
 
-	return {
-		// Public variables and functions
+  return {
+    // Public variables and functions
 
-		removeDuplicates: function (arr){
-			// Removes duplicate entries in an array of 
+    removeDuplicates: function (arr){
+      // Removes duplicate entries in an array of 
       // numbers and/or strings
-			if (!(arr instanceof Array) || arr.length < 1){
-				throw new Error('This is not an array.');
-				return arr;
-			}
+      if (!(arr instanceof Array) || arr.length < 1){
+        throw new Error('This is not an array.');
+        return arr;
+      }
 
-			var arrayMap = {};
+      var arrayMap = {};
 
-			for (var i = 0; i < arr.length; i++){
-				arrayMap[arr[i]] = arr[i];
-			}
+      for (var i = 0; i < arr.length; i++){
+        arrayMap[arr[i]] = arr[i];
+      }
 
-			arr.splice(0, arr.length);
+      arr.splice(0, arr.length);
 
-			for (var p in arrayMap){
-				arr.push(arrayMap[p]);
-			}
-			return arr;
-		}
-	}
-}
+      for (var p in arrayMap){
+        arr.push(arrayMap[p]);
+      }
+      return arr;
+    }
+  }
+})();
